8f513f5cbd9e08eb3d1f59bc5622ac924b0225cf
Daniël van Drunen <daniel@pixely.me>
2017-07-08 01:06:23 +0300

    Reprogram object structure to support existing driver-route configurations

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -8,8 +8,33 @@ class Home extends React.Component {
         super()
 
         this.state = {
-            routes: ['A', 'B', 'C'],
-            drivers: ['Ali', 'John'],
+            routes: [
+                {
+                    label: 'Alpha',
+                    driver_id: 1,
+                    hour: 10
+                },
+                {
+                    label: 'Beta',
+                    driver_id: null,
+                    hour: null
+                },
+                {
+                    label: 'Charlie',
+                    driver_id: null,
+                    hour: null
+                }
+            ],
+            drivers: [
+                {
+                    id: 1,
+                    name: 'Ali'
+                },
+                {
+                    id: 2,
+                    name: 'Mark'
+                }
+            ],
             hours: [9, 10, 11, 12, 13]
         }
     }
@@ -36,24 +61,34 @@ class Home extends React.Component {
                 <h1>Drag & Drop</h1>
                 <ul>
                     {this.state.routes.map((route) => {
-                        return (
-                            <li>
-                                <span draggable="true" onDragStart={this.drag} id={route}>Route {route}</span>
-                            </li>
-                        )
+                        if (!route.driver_id && !route.hour) {
+                            return (
+                                <li>
+                                    <span draggable="true" onDragStart={this.drag} id={route.label}>Route {route.label}</span>
+                                </li>
+                            )
+                        }
                     })}
                 </ul>
                 {this.state.drivers.map((driver) => {
                     return (
                         <div>
-                            <h2>{driver}'s shift</h2>
+                            <h2>{driver.name}'s shift</h2>
                             <table>
                                 <tbody>
                                     {this.state.hours.map((hour) => {
                                         return (
                                             <tr>
                                                 <td>{hour}:00</td>
-                                                <td onDrop={this.drop} onDragOver={this.allowDrop}></td>
+                                                <td onDrop={this.drop} onDragOver={this.allowDrop}>
+                                                    {this.state.routes.map((route) => {
+                                                        if (route.driver_id === driver.id && route.hour === hour) {
+                                                            return (
+                                                                <span draggable="true" onDragStart={this.drag} id={route.label}>Route {route.label}</span>
+                                                            )
+                                                        }
+                                                    })}
+                                                </td>
                                             </tr>
                                         )
                                     })}
