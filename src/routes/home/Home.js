720470b857c1c7aa0837718c99f45921462017f7
Daniël van Drunen <daniel@pixely.me>
2017-07-09 16:37:20 +0300

    Get rid of replaced code

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './Home.css';
+
+import styles from './Home.css';
 
 class Home extends React.Component {
     constructor() {
@@ -102,34 +102,4 @@ class Home extends React.Component {
     }
 }
 
-/* class Home extends React.Component {
-  static propTypes = {
-    news: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      link: PropTypes.string.isRequired,
-      content: PropTypes.string,
-    })).isRequired,
-  };
-
-  render() {
-    return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <h1>React.js News</h1>
-          {this.props.news.map(item => (
-            <article key={item.link} className={s.newsItem}>
-              <h1 className={s.newsTitle}><a href={item.link}>{item.title}</a></h1>
-              <div
-                className={s.newsDesc}
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{ __html: item.content }}
-              />
-            </article>
-          ))}
-        </div>
-      </div>
-    );
-  }
-} */
-
-export default withStyles(s)(Home);
+export default withStyles(styles)(Home);
